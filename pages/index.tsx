54f4de8e61bc78df609a14eea2a19494936c19f0
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2021-11-20 21:57:48 +0100

    Tweak grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,14 +35,12 @@ const Home: NextPage<Props> = ({ v1, v2, cdktf, cdk8s }) => {
             <div className="relative">
               <div className="absolute inset-0 h-1/2 bg-gray-50" />
               <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="max-w-4xl mx-auto">
-                  <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-4">
-                    <CdkRelease name="CDK" release={v1} />
-                    <CdkRelease name="CDK v2" release={v2} />
-                    <CdkRelease name="cdktf" release={cdktf} />
-                    <CdkRelease name="cdk8s" release={cdk8s} />
-                  </dl>
-                </div>
+                <dl className="rounded-lg bg-white shadow-lg md:grid md:grid-cols-4">
+                  <CdkRelease name="CDK" release={v1} />
+                  <CdkRelease name="CDK v2" release={v2} />
+                  <CdkRelease name="cdktf" release={cdktf} />
+                  <CdkRelease name="cdk8s" release={cdk8s} />
+                </dl>
               </div>
             </div>
           </div>
@@ -58,7 +56,7 @@ const CdkRelease = ({ name, release }: { name: string; release: Release }) => {
   const publishedAt = new Date(release.published_at!)
 
   return (
-    <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
+    <div className="flex flex-col border-b border-gray-100 p-6 text-center md:border-0 md:border-r">
       <dt className="text-5xl font-extrabold text-indigo-600">{name}</dt>
       <dd className="mt-2 text-lg leading-6 font-medium text-gray-500 underline">
         <a href={release.html_url} target="_blank" rel="noopener noreferrer">
