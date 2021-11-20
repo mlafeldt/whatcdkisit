11e30c1fa169d98e4516cb387cb1815fd38d7610
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2021-11-20 19:07:50 +0100

    Make use of Tailwind UI
    https://tailwindui.com/components/marketing/sections/stats-sections

--- pages/cdk.tsx
+++ pages/cdk.tsx
@@ -13,30 +13,48 @@ type Props = {
 
 const Page: NextPage<Props> = ({ v1, v2, tf }) => {
   return (
-    <div>
-      <h2>
-        CDK {v1.tag_name} | {new Date(v1.published_at!).toDateString()} |{' '}
-        <a href={v1.html_url} target="_blank" rel="noopener noreferrer">
-          changelog
-        </a>
-      </h2>
-      <h2>
-        CDK {v2.tag_name} | {new Date(v2.published_at!).toDateString()} |{' '}
-        <a href={v2.html_url} target="_blank" rel="noopener noreferrer">
-          changelog
-        </a>
-      </h2>
-      <h2>
-        CDKTF {tf.tag_name} | {new Date(tf.published_at!).toDateString()} |{' '}
-        <a href={tf.html_url} target="_blank" rel="noopener noreferrer">
-          changelog
-        </a>
-      </h2>
+    <div className="bg-gray-50 pt-12 sm:pt-16">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-4xl mx-auto text-center">
+          <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">What CDK is it?</h2>
+          <p className="mt-3 text-xl text-gray-500 sm:mt-4">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus repellat laudantium.
+          </p>
+        </div>
+      </div>
+      <div className="mt-10 pb-12 bg-white sm:pb-16">
+        <div className="relative">
+          <div className="absolute inset-0 h-1/2 bg-gray-50" />
+          <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-4xl mx-auto">
+              <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
+                <CdkRelease name="CDK" release={v1} />
+                <CdkRelease name="CDK v2" release={v2} />
+                <CdkRelease name="CDKTF" release={tf} />
+              </dl>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Page
+const CdkRelease = ({ name, release }: { name: string; release: Release }) => {
+  return (
+    <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
+      <dt className="text-5xl font-extrabold text-indigo-600">{name}</dt>
+      <dd className="mt-2 text-lg leading-6 font-medium text-gray-500 underline">
+        <a href={release.html_url} target="_blank" rel="noopener noreferrer">
+          {release.tag_name}
+        </a>
+      </dd>
+      <dd className="mt-2 text-lg leading-6 font-medium text-gray-500">
+        {new Date(release.published_at!).toDateString()}
+      </dd>
+    </div>
+  )
+}
 
 export const getStaticProps: GetStaticProps = async () => {
   const { data: cdkReleases } = await request('GET /repos/{owner}/{repo}/releases', {
@@ -62,3 +80,5 @@ export const getStaticProps: GetStaticProps = async () => {
     revalidate: 600, // update every 10 minutes
   }
 }
+
+export default Page
