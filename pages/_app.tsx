54be47fa3b05fcd82765ff92c8b9b308c2ff16f4
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2021-11-21 18:36:44 +0100

    Set up Fathom Analytics
    https://vercel.com/guides/deploying-nextjs-using-fathom-analytics-with-vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import * as Fathom from 'fathom-client'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load('TTTBKERK', {
+      url: 'https://thirteen-skilled.whatcdkisit.com/script.js',
+      includedDomains: ['whatcdkisit.com'],
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
