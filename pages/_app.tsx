12dab7e26de269d474bce1b9e5e2001262b15149
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2023-10-09 15:38:06 +0200

    Remove Fathom Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,7 @@
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-import * as Fathom from 'fathom-client'
 
 function App({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-
-  useEffect(() => {
-    // Initialize Fathom when the app loads
-    Fathom.load('TTTBKERK', {
-      includedDomains: ['whatcdkisit.vercel.app'],
-    })
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview()
-    }
-
-    // Record a pageview when route changes
-    router.events.on('routeChangeComplete', onRouteChangeComplete)
-
-    // Unassign event listener
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete)
-    }
-  }, [router.events])
-
   return <Component {...pageProps} />
 }
 
