197a0dfd4a957e1735673e5853abae64dfae8ef8
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2025-03-21 11:08:06 +0100

    Update deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
+
+import '../styles/global.css'
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
